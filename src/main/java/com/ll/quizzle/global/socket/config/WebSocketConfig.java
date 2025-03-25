2d1a7c74242f6ad2c6a1f578a5cb7d46d2155b03
jurio5 <sia5492@naver.com>
2025-03-25 19:05:24 +0900

    [comment] WebSocketConfig 주석 보충 설명

--- src/main/java/com/ll/quizzle/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/ll/quizzle/global/socket/config/WebSocketConfig.java
@@ -22,14 +22,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // topic 부분은 topic 된 API의 구독자에게 브로드 캐스트 메시지를 전달합니다.
-        // queue 부분은 user와 혼합해서 사용도 가능하고, queue 자체로도 상황에 맞게 사용을 할 수 있습니다.
+        // topic 부분은 topic 에 포함 된 API 의 구독자에게 브로드 캐스트 메시지를 전달합니다.
+        // queue 부분은 user 와 혼합해서 사용도 가능하고, queue 자체로도 상황에 맞게 사용을 할 수 있습니다.
         registry.enableSimpleBroker("/topic", "/queue");
 
         // 클라이언트 -> 서버로 메시지를 보낼 때 사용하는 접두사 입니다.
         registry.setApplicationDestinationPrefixes("/app");
 
-        // 개인 메시지를 위한 접두사 입니다. (보통 queue와 혼합하여 사용합니다.)
+        // 개인 메시지를 위한 접두사 입니다. (보통 queue 와 혼합하여 사용합니다.)
         registry.setUserDestinationPrefix("/user");
     }
 
@@ -37,6 +37,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(endpoint)
                 .setAllowedOriginPatterns(allowedOrigins)
-                .withSockJS(); // WebSocket을 지원하지 않는 브라우저를 위해 추가
+                .withSockJS(); // WebSocket 을 지원하지 않는 브라우저를 위해 추가
     }
 } 
