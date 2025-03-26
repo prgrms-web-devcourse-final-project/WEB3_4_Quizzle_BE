599a5e42e9bc99063874a3225262e85d943d9e8f
kwonkiyong0059 <kkoung425@gmail.com>
2025-03-26 16:26:54 +0900

    [feat] PointReason enum에 defaultAmount 필드 추가

--- src/main/java/com/ll/quizzle/domain/point/type/PointReason.java
+++ src/main/java/com/ll/quizzle/domain/point/type/PointReason.java
@@ -1,18 +1,23 @@
 package com.ll.quizzle.domain.point.type;
 
 public enum PointReason {
-	NICKNAME_CHANGE("닉네임 변경"),
-	LEVEL_UP("레벨업 보상"),
-	// 추가 가능
-	;
+	NICKNAME_CHANGE("닉네임 변경", -100),
+	LEVEL_UP("레벨업 보상", +500);
 
 	private final String description;
+	private final int defaultAmount;
 
-	PointReason(String description) {
+	PointReason(String description, int defaultAmount) {
 		this.description = description;
+		this.defaultAmount = defaultAmount;
 	}
 
 	public String getDescription() {
 		return description;
 	}
+
+	public int getDefaultAmount() {
+		return defaultAmount;
+	}
 }
+
