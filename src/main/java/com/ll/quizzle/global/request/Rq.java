cd8ad6e629dfbd7c76db60888a1566991685483f
kwonkiyong0059 <kkoung425@gmail.com>
2025-03-28 11:51:42 +0900

    [feat] 인증된 사용자와 리소스 소유자 일치 검증 로직 추가

--- src/main/java/com/ll/quizzle/global/request/Rq.java
+++ src/main/java/com/ll/quizzle/global/request/Rq.java
@@ -1,34 +1,45 @@
 package com.ll.quizzle.global.request;
 
-import com.ll.quizzle.domain.member.entity.Member;
-import com.ll.quizzle.domain.member.repository.MemberRepository;
-import com.ll.quizzle.global.exceptions.ErrorCode;
-import com.ll.quizzle.global.security.oauth2.dto.SecurityUser;
-import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
-import java.util.Optional;
+import com.ll.quizzle.domain.member.entity.Member;
+import com.ll.quizzle.domain.member.repository.MemberRepository;
+import com.ll.quizzle.global.exceptions.ErrorCode;
+import com.ll.quizzle.global.security.oauth2.dto.SecurityUser;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequestScope
 @RequiredArgsConstructor
 public class Rq {
-    private final MemberRepository memberRepository;
-    private Member actor;
+	private final MemberRepository memberRepository;
+	private Member actor;
 
-    public Member getActor() {
-        if (actor == null) {
-            actor = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
-                    .map(Authentication::getPrincipal)
-                    .filter(principal -> principal instanceof SecurityUser)
-                    .map(principal -> (SecurityUser) principal)
-                    .flatMap(securityUser -> memberRepository.findByEmail(securityUser.getEmail()))
-                    .orElseThrow(ErrorCode.UNAUTHORIZED::throwServiceException);
-        }
+	public Member getActor() {
+		if (actor == null) {
+			actor = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+				.map(Authentication::getPrincipal)
+				.filter(principal -> principal instanceof SecurityUser)
+				.map(principal -> (SecurityUser)principal)
+				.flatMap(securityUser -> memberRepository.findByEmail(securityUser.getEmail()))
+				.orElseThrow(ErrorCode.UNAUTHORIZED::throwServiceException);
+		}
 
-        return actor;
-    }
+		return actor;
+	}
+
+	public void assertIsOwner(Long targetMemberId) {
+		Member actor = getActor();
+
+		if (!actor.getId().equals(targetMemberId)) {
+			throw ErrorCode.FORBIDDEN_ACCESS.throwServiceException();
+		}
+	}
+
 }
