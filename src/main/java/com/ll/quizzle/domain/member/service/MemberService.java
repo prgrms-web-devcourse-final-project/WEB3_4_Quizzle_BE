ede2ff02a45829e749b9e72ba33461cd94706d7d
bsh <paek1127@gmail.com>
2025-03-29 00:30:37 +0900

    refactor: CookieUtil 사용하도록 저장 로직 수정

--- src/main/java/com/ll/quizzle/domain/member/service/MemberService.java
+++ src/main/java/com/ll/quizzle/domain/member/service/MemberService.java
@@ -7,6 +7,7 @@ import com.ll.quizzle.global.jwt.dto.JwtProperties;
 import com.ll.quizzle.global.request.Rq;
 import com.ll.quizzle.global.response.RsData;
 import com.ll.quizzle.global.security.oauth2.repository.OAuthRepository;
+import com.ll.quizzle.standard.util.CookieUtil;
 import com.ll.quizzle.standard.util.Ut;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -87,24 +88,37 @@ public class MemberService {
 
     private void addAuthCookies(HttpServletResponse response, GeneratedToken tokens, Member member) {
         // Access Token 쿠키
-        Cookie accessTokenCookie = new Cookie("access_token", tokens.accessToken());
-        accessTokenCookie.setPath("/");
-        accessTokenCookie.setHttpOnly(true);
-        response.addCookie(accessTokenCookie);
+        CookieUtil.addCookie(
+                response,
+                "access_token",
+                tokens.accessToken(),
+                (int) jwtProperties.getAccessTokenExpiration(),
+                true,
+                true
+        );
 
         // Refresh Token 쿠키
-        Cookie refreshTokenCookie = new Cookie("refresh_token", tokens.refreshToken());
-        refreshTokenCookie.setPath("/");
-        refreshTokenCookie.setHttpOnly(true);
-        response.addCookie(refreshTokenCookie);
+        CookieUtil.addCookie(
+                response,
+                "refresh_token",
+                tokens.refreshToken(),
+                (int) jwtProperties.getRefreshTokenExpiration(),
+                true,
+                true
+        );
 
         // Role 쿠키
         Map<String, Object> roleData = new HashMap<>();
         roleData.put("role", member.getUserRole());
 
-        Cookie roleCookie = new Cookie("role", URLEncoder.encode(Ut.json.toString(roleData), StandardCharsets.UTF_8));
-        roleCookie.setPath("/");
-        response.addCookie(roleCookie);
+        CookieUtil.addCookie(
+                response,
+                "role",
+                URLEncoder.encode(Ut.json.toString(roleData), StandardCharsets.UTF_8),
+                (int) jwtProperties.getAccessTokenExpiration(),
+                false,
+                true
+        );
     }
 
     public void logout(HttpServletRequest request, HttpServletResponse response) {
