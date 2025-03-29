91ea98ee57ade1d21d2ca32587f3608761cc3a43
leesangeok <118970618+leesangeok@users.noreply.github.com>
2025-03-29 11:01:25 +0900

    Revert "[FEAT] [BE] Quiz 퀴즈 채점 기능 구현 "

--- src/main/java/com/ll/quizzle/domain/quiz/dto/QuizGenerationResponseDTO.java
+++ src/main/java/com/ll/quizzle/domain/quiz/dto/QuizGenerationResponseDTO.java
@@ -1,4 +1,4 @@
-package com.ll.quizzle.domain.quiz.dto.generation;
+package com.ll.quizzle.domain.quiz.dto;
 
 import java.util.Map;
 
--- src/main/java/com/ll/quizzle/domain/quiz/dto/submission/QuizSubmissionResultDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ll.quizzle.domain.quiz.dto.submission;
-
-public record QuizSubmissionResultDTO(
-        int questionNumber,
-        boolean correct,
-        String correctAnswer,
-        String message
-) {
-}
