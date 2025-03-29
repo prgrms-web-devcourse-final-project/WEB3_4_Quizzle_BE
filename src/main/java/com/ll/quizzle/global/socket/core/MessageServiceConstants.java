71a7bc15efc5ea3ee16d503aa0988ed6eb618c6c
jurio5 <sia5492@naver.com>
2025-03-29 17:00:28 +0900

    [delete] 프로퍼티 상수 벨류 제거

--- src/main/java/com/ll/quizzle/global/socket/core/MessageServiceConstants.java
+++ src/main/java/com/ll/quizzle/global/socket/core/MessageServiceConstants.java
@@ -4,8 +4,4 @@ package com.ll.quizzle.global.socket.core;
 public class MessageServiceConstants {
     public static final String PROVIDER_STOMP = "stomp";
     public static final String PROVIDER_REDIS = "redis";
-    
-    public static final String CONFIG_DEFAULT_PROVIDER = "quizzle.messaging.provider";
-    public static final String CONFIG_ROOM_PROVIDER = "quizzle.messaging.room.provider";
-    public static final String CONFIG_CHAT_PROVIDER = "quizzle.messaging.chat.provider";
 } 
