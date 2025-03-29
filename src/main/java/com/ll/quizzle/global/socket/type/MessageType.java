9bcc1670224ef3eaaf9635fc6ce5c9e0cdd35e2f
jurio5 <sia5492@naver.com>
2025-03-29 17:31:38 +0900

    [refactor] 기존 타입 주석 처리 되던 부분 필드로 처리

--- src/main/java/com/ll/quizzle/global/socket/type/MessageType.java
+++ src/main/java/com/ll/quizzle/global/socket/type/MessageType.java
@@ -1,12 +1,19 @@
 package com.ll.quizzle.global.socket.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 채팅 메시지 타입
  */
+@Getter
+@RequiredArgsConstructor
 public enum MessageType {
-    CHAT,           // 일반 채팅
-    JOIN,           // 입장 메시지
-    LEAVE,          // 퇴장 메시지
-    SYSTEM,         // 시스템 메시지
-    WHISPER         // 귓속말
+    CHAT("일반 채팅"),
+    JOIN("입장 메시지"),
+    LEAVE("퇴장 메시지"),
+    SYSTEM("시스템 메시지"),
+    WHISPER("귓속말");
+    
+    private final String description;
 } 
