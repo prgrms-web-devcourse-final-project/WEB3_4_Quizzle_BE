75035b73eab6554fe074d1702900e4a0c161c57c
jurio5 <sia5492@naver.com>
2025-03-31 14:54:53 +0900

    [feat] 대규모 트래픽 방지를 위한 후크 메서드 추가

--- src/main/java/com/ll/quizzle/global/socket/config/WebSocketConfig.java
+++ src/main/java/com/ll/quizzle/global/socket/config/WebSocketConfig.java
@@ -1,9 +1,5 @@
 package com.ll.quizzle.global.socket.config;
 
-import com.ll.quizzle.global.socket.interceptor.StompChannelInterceptor;
-import com.ll.quizzle.global.socket.interceptor.WebSocketHandshakeInterceptor;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -12,6 +8,13 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+import com.ll.quizzle.global.socket.interceptor.StompChannelInterceptor;
+import com.ll.quizzle.global.socket.interceptor.WebSocketHandshakeInterceptor;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
@@ -56,5 +59,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(channelInterceptor);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000)
+                   .setSendBufferSizeLimit(512 * 1024)
+                   .setMessageSizeLimit(128 * 1024);
     }
 } 
