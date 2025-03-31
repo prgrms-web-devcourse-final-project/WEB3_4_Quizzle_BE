dca7c7fe18b3e2aaccae816b34be471f90763307
kwonkiyong0059 <kkoung425@gmail.com>
2025-03-31 16:38:57 +0900

    [refactor] 멘토링 피드백 반영

--- src/main/java/com/ll/quizzle/global/exceptions/ErrorCode.java
+++ src/main/java/com/ll/quizzle/global/exceptions/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     TOKEN_LOGGED_OUT(HttpStatus.UNAUTHORIZED, "로그아웃된 토큰입니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST, "리프레시 토큰을 찾을 수 없습니다."),
     REFRESH_TOKEN_INVALID(HttpStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다."),
+	TOKEN_INFO_NOT_FOUND(HttpStatus.BAD_REQUEST, "토큰 정보가 없습니다."),
 
     // member + oauth
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원이 존재하지 않습니다."),
--- src/test/java/com/ll/quizzle/factory/PointTestFactory.java
+++ src/test/java/com/ll/quizzle/factory/PointTestFactory.java
@@ -4,17 +4,29 @@ import com.ll.quizzle.domain.member.entity.Member;
 import com.ll.quizzle.domain.point.entity.Point;
 import com.ll.quizzle.domain.point.repository.PointRepository;
 import com.ll.quizzle.domain.point.type.PointReason;
+import com.ll.quizzle.domain.point.type.PointType;
 
 public class PointTestFactory {
 
 	public static void createRewardPoint(Member member, PointRepository repo, int amount) {
 		member.increasePoint(amount);
-		repo.save(Point.gain(member, amount, PointReason.LEVEL_UP));
+		Point point = Point.builder()
+			.member(member)
+			.amount(amount)
+			.type(PointType.REWARD)
+			.reason(PointReason.LEVEL_UP)
+			.build();
+		repo.save(point);
 	}
 
 	public static void createUsePoint(Member member, PointRepository repo, int amount) {
 		member.decreasePoint(amount);
-		repo.save(Point.use(member, amount, PointReason.NICKNAME_CHANGE));
+		Point point = Point.builder()
+			.member(member)
+			.amount(-amount)
+			.type(PointType.USE)
+			.reason(PointReason.NICKNAME_CHANGE)
+			.build();
+		repo.save(point);
 	}
-
 }
