0e731441e7e9a9a7395e0ee478f0fe35d5544874
jurio5 <sia5492@naver.com>
2025-04-01 17:38:44 +0900

    [feat] Main 테스트 컨트롤러 추가

---
+++ src/main/java/com/ll/quizzle/domain/MainController.java
@@ -0,0 +1,13 @@
+package com.ll.quizzle.domain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main() {
+        return "EC2 서버에 배포 테스트";
+    }
+}
