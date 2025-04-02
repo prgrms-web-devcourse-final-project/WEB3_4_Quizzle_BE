7689707793715f3024781812a834cf674d723845
jurio5 <sia5492@naver.com>
2025-04-02 18:40:08 +0900

    [feat] 방 상태 타입 추가

--- src/main/java/com/ll/quizzle/domain/room/type/RoomStatus.java
+++ src/main/java/com/ll/quizzle/domain/room/type/RoomStatus.java
@@ -1,4 +1,14 @@
 package com.ll.quizzle.domain.room.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum RoomStatus {
+    WAITING("대기 중"),
+    IN_GAME("게임 중"),
+    FINISHED("게임 종료");
+
+    private final String description;
 }
