0cdc1591d36a9d0d0c84a49173a7a6074a23b339
jurio5 <sia5492@naver.com>
2025-04-02 18:41:18 +0900

    [feat] 서브 카테고리 타입 추가

--- src/main/java/com/ll/quizzle/domain/room/type/SubCategory.java
+++ src/main/java/com/ll/quizzle/domain/room/type/SubCategory.java
@@ -1,4 +1,24 @@
 package com.ll.quizzle.domain.room.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum SubCategory {
+    PHYSICS("물리학"),
+    CHEMISTRY("화학"),
+    BIOLOGY("생물학"),
+    
+    WORLD_HISTORY("세계사"),
+    KOREAN_HISTORY("한국사"),
+    
+    KOREAN("한국어"),
+    ENGLISH("영어"),
+    
+    CURRENT_AFFAIRS("시사"),
+    CULTURE("문화"),
+    SPORTS("스포츠");
+    
+    private final String description;
 }
