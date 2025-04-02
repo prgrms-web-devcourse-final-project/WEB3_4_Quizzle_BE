1a3159c370c012ad74b603f5c3e47ab8a484e1b2
jurio5 <sia5492@naver.com>
2025-04-02 15:03:00 +0900

    [refactor] 웹 소켓 chat request 네이밍 일관성 유지

---
+++ src/main/java/com/ll/quizzle/global/socket/dto/request/WebSocketChatMessageRequest.java
@@ -0,0 +1,27 @@
+package com.ll.quizzle.global.socket.dto.request;
+
+import com.ll.quizzle.global.socket.type.MessageType;
+
+/**
+ * 클라이언트로부터 받는 채팅 메시지 요청을 위한 DTO
+ */
+public record WebSocketChatMessageRequest(
+    MessageType type,
+    String content,
+    String senderId,
+    String senderName,
+    long timestamp,
+    String roomId
+) {
+
+    public static WebSocketChatMessageRequest of(
+            MessageType type,
+            String content,
+            String senderId,
+            String senderName,
+            long timestamp,
+            String roomId
+    ) {
+        return new WebSocketChatMessageRequest(type, content, senderId, senderName, timestamp, roomId);
+    }
+} 
