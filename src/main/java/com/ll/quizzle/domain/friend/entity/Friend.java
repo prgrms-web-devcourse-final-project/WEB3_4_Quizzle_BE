07585377023da5d3fcaa42b1ad73f065d159f30c
SH Baek <paek1127@gmail.com>
2025-04-07 00:23:01 +0900

    feat: friend, friendOffer entity 추가

---
+++ src/main/java/com/ll/quizzle/domain/friend/entity/Friend.java
@@ -0,0 +1,43 @@
+package com.ll.quizzle.domain.friend.entity;
+
+import com.ll.quizzle.domain.member.entity.Member;
+import com.ll.quizzle.global.jpa.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Friend extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "friend_id", nullable = false)
+    private Member friend;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @Builder
+    private Friend(Member member, Member friend) {
+        this.member = member;
+        this.friend = friend;
+    }
+
+    public static Friend create(Member member, Member friend) {
+        return Friend.builder()
+                .member(member)
+                .friend(friend)
+                .build();
+    }
+}
