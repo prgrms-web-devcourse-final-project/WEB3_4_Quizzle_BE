c3fa82ba21d03e581f304f8b01982a248d192786
jurio5 <sia5492@naver.com>
2025-04-07 20:13:55 +0900

    [feat] 룸 JPA 방 Id 조회 및 컬렉션 테이블 PlayerId 조회 추가

--- src/main/java/com/ll/quizzle/domain/room/repository/RoomRepository.java
+++ src/main/java/com/ll/quizzle/domain/room/repository/RoomRepository.java
@@ -1,11 +1,20 @@
 package com.ll.quizzle.domain.room.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.ll.quizzle.domain.room.entity.Room;
 import com.ll.quizzle.domain.room.type.RoomStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> findByStatusNot(RoomStatus status);
+    
+    Optional<Room> findRoomById(Long id);
+    
+    @Query("SELECT r FROM Room r JOIN r.players p WHERE p = :playerId")
+    List<Room> findRoomsByPlayerId(@Param("playerId") Long playerId);
 }
