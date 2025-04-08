2ae4c1060c1e4feedea08e1a408c886d95127cfe
jurio5 <sia5492@naver.com>
2025-04-08 12:17:15 +0900

    [refactor] 기본적으로 스프링에 위임하던 부분 제거 및 팩토리 메서드 패턴으로 변경

--- src/main/java/com/ll/quizzle/global/socket/session/WebSocketSessionRegistry.java
+++ src/main/java/com/ll/quizzle/global/socket/session/WebSocketSessionRegistry.java
@@ -1,6 +1,5 @@
 package com.ll.quizzle.global.socket.session;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -8,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 웹 소켓 세션 관리 전략 (추후 확장성을 고려)
+ * 웹 소켓 세션 관리 전략 팩토리 (추후 확장성을 고려)
  * 예로들어 새로운 이벤트 세션 즉, 카프카, RabbitMQ 등으로 대체할 경우를 대비
  */
 @Slf4j
@@ -16,26 +15,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class WebSocketSessionRegistry {
 
-    @Qualifier("inMemoryWebSocketSessionManager")
     private final WebSocketSessionManager inMemorySessionManager;
-
-    @Qualifier("redisWebSocketSessionManager")
     private final WebSocketSessionManager redisSessionManager;
 
-    /**
-     * SuppressWarnings 은 불명확해서 붙이기 싫은데 IDE 쪽에서 Value 가 불리언보다 먼저 타입 지정이되어 오류가 발생함
-     * final 필드를 빼면 오류가 발생하진 않지만, RequiredArgsConstructor 가 작동하지 않을거라 일단 SuppressWarnings 처리
-     */
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Value("${app.websocket.use-distributed-sessions:false}")
-    private final boolean useDistributedSessions;
+
+    @Value("${app.websocket.use-distributed-sessions:true}")
+    private boolean useDistributedSessions;
     
+
     public WebSocketSessionManager getSessionManager() {
         if (useDistributedSessions) {
             log.debug("Redis 기반 세션 매니저 사용");
             return redisSessionManager;
         } else {
-            log.debug("InMemory 기반 세션 매니저 사용");
+            log.debug("인메모리 기반 세션 매니저 사용");
             return inMemorySessionManager;
         }
     }
