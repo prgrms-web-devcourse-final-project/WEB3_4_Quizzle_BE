5145a1b7a460e5d6a8819306ca090abed9f36c3c
jurio5 <sia5492@naver.com>
2025-04-08 16:14:49 +0900

    [rename] 분산 락 추상 인터페이스 네이밍 변경

--- src/main/java/com/ll/quizzle/global/redis/lock/DistributedLockService.java
+++ src/main/java/com/ll/quizzle/global/redis/lock/DistributedLockService.java
@@ -5,12 +5,11 @@ import java.util.concurrent.TimeUnit;
 /**
  * 테스트 코드에서 RedisLockService 를 Mocking 하기 위해 인터페이스 분리 (그 외 확장성(ex.zookeeper)과 유연성 고려)
  */
-public interface RedisLockService {
+public interface DistributedLockService {
     /**
      * 분산 락 획득
      */
     boolean acquireLock(String key, String value, long waitTime, TimeUnit timeUnit);
-    
     /**
      * 분산 락 해제
      */
