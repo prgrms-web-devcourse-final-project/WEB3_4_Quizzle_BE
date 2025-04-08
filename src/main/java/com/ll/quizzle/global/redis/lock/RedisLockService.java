8ce09583094885ee96baccd6dd0312749749c739
jurio5 <sia5492@naver.com>
2025-04-08 16:03:58 +0900

    [feat] 분산 락 인터페이스 분리

---
+++ src/main/java/com/ll/quizzle/global/redis/lock/RedisLockService.java
@@ -0,0 +1,18 @@
+package com.ll.quizzle.global.redis.lock;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 테스트 코드에서 RedisLockService 를 Mocking 하기 위해 인터페이스 분리 (그 외 확장성(ex.zookeeper)과 유연성 고려)
+ */
+public interface RedisLockService {
+    /**
+     * 분산 락 획득
+     */
+    boolean acquireLock(String key, String value, long waitTime, TimeUnit timeUnit);
+    
+    /**
+     * 분산 락 해제
+     */
+    boolean releaseLock(String key, String value);
+} 
