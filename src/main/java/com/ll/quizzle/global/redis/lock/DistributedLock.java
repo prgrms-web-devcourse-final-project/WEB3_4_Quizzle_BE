809c27e2cac6623675a84d341837ebafcfc1bc91
jurio5 <sia5492@naver.com>
2025-04-09 09:47:44 +0900

    [comment] 분산 락 커스텀 어노테이션 주석 제거

--- src/main/java/com/ll/quizzle/global/redis/lock/DistributedLock.java
+++ src/main/java/com/ll/quizzle/global/redis/lock/DistributedLock.java
@@ -8,18 +8,10 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
-    /**
-     * SpEL 표현식으로 동적 키를 생성하는 값
-     */
+
     String key();
     
-    /**
-     * 락의 유효 시간
-     */
     long leaseTime() default 5000;
-    
-    /**
-     * 락 획득 시도 시간
-     */
+
     long waitTime() default 3000;
 } 
