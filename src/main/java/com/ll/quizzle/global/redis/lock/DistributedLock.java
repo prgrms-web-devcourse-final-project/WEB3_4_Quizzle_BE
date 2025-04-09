30ced6c35c1aa2ce8c013b7a414da62550119cdd
jurio5 <sia5492@naver.com>
2025-04-09 09:50:03 +0900

    [chor] 분산 락 커스텀 어노테이션 라인 정리

--- src/main/java/com/ll/quizzle/global/redis/lock/DistributedLock.java
+++ src/main/java/com/ll/quizzle/global/redis/lock/DistributedLock.java
@@ -8,9 +8,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DistributedLock {
-
     String key();
-    
+
     long leaseTime() default 5000;
 
     long waitTime() default 3000;
