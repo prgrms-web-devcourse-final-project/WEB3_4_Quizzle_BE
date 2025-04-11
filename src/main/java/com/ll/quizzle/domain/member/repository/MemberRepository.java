cb74f88ff529c871fc3f88fe4e415660bc0f235a
jurio5 <sia5492@naver.com>
2025-04-12 00:19:28 +0900

    [feat] Member Data JPA 전체 유저 내림차순 정렬 추가

--- src/main/java/com/ll/quizzle/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ll/quizzle/domain/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.ll.quizzle.domain.member.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findById(Long id);
 
     boolean existsByNickname(String nickname);
+    
+    List<Member> findAllByOrderByExpDesc();
 }
