48b3c8aeacf890949489a839418b9273426138f4
jhs512 <jangka512@gmail.com>
2025-04-11 11:45:15 +0900

    make sample rooms

--- src/main/java/com/ll/quizzle/global/initData/BaseInitData.java
+++ src/main/java/com/ll/quizzle/global/initData/BaseInitData.java
@@ -1,7 +1,21 @@
-package com.ll.quizzle.global.initdata;
+package com.ll.quizzle.global.initData;
 
-import static com.ll.quizzle.global.exceptions.ErrorCode.*;
-
+import com.ll.quizzle.domain.avatar.entity.Avatar;
+import com.ll.quizzle.domain.avatar.repository.AvatarRepository;
+import com.ll.quizzle.domain.avatar.type.AvatarStatus;
+import com.ll.quizzle.domain.member.entity.Member;
+import com.ll.quizzle.domain.member.repository.MemberRepository;
+import com.ll.quizzle.domain.member.type.Role;
+import com.ll.quizzle.domain.room.dto.request.RoomCreateRequest;
+import com.ll.quizzle.domain.room.service.RoomService;
+import com.ll.quizzle.domain.room.type.AnswerType;
+import com.ll.quizzle.domain.room.type.Difficulty;
+import com.ll.quizzle.domain.room.type.MainCategory;
+import com.ll.quizzle.domain.room.type.SubCategory;
+import com.ll.quizzle.domain.system.service.SystemService;
+import com.ll.quizzle.global.security.oauth2.entity.OAuth;
+import com.ll.quizzle.global.security.oauth2.repository.OAuthRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +24,10 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ll.quizzle.domain.avatar.entity.Avatar;
-import com.ll.quizzle.domain.avatar.repository.AvatarRepository;
-import com.ll.quizzle.domain.avatar.type.AvatarStatus;
-import com.ll.quizzle.domain.member.entity.Member;
-import com.ll.quizzle.domain.member.repository.MemberRepository;
-import com.ll.quizzle.domain.member.type.Role;
-import com.ll.quizzle.domain.system.service.SystemService;
-import com.ll.quizzle.global.security.oauth2.entity.OAuth;
-import com.ll.quizzle.global.security.oauth2.repository.OAuthRepository;
+import java.util.ArrayList;
+import java.util.List;
 
-import lombok.RequiredArgsConstructor;
+import static com.ll.quizzle.global.exceptions.ErrorCode.AVATAR_NOT_FOUND;
 
 @Profile("dev")
 @Component
@@ -30,6 +37,7 @@ public class BaseInitData {
     private final OAuthRepository oAuthRepository;
     private final AvatarRepository avatarRepository;
     private final SystemService systemService;
+    private final RoomService roomService;
 
     @Autowired
     @Lazy
@@ -51,8 +59,9 @@ public class BaseInitData {
         }
 
         Avatar defaultAvatar = avatarRepository.findByFileName("새콩이")
-            .orElseThrow(AVATAR_NOT_FOUND::throwServiceException);
+                .orElseThrow(AVATAR_NOT_FOUND::throwServiceException);
 
+        List<Member> members = new ArrayList<>();
 
         for (int i = 1; i <= 10; i++) {
             String nickname = "test" + i;
@@ -60,11 +69,29 @@ public class BaseInitData {
             String provider = (i % 2 == 0) ? "google" : "kakao";
 
             Member member = Member.create(nickname, email, defaultAvatar);
+            members.add(member);
 
             OAuth oauth = OAuth.create(member, provider, String.valueOf(i));
 
             memberRepository.save(member);
             oAuthRepository.save(oauth);
+        }
+
+        for (Member owner : members) {
+            roomService.createRoom(
+                    owner.getId(),
+                    new RoomCreateRequest(
+                            "제목",
+                            2,
+                            Difficulty.EASY,
+                            MainCategory.HISTORY,
+                            SubCategory.WORLD_HISTORY,
+                            AnswerType.MULTIPLE_CHOICE,
+                            11,
+                            null,
+                            false
+                    )
+            );
         }
     }
 
@@ -80,7 +107,7 @@ public class BaseInitData {
         }
 
         Avatar defaultAvatar = avatarRepository.findByFileName("새콩이")
-            .orElseThrow(AVATAR_NOT_FOUND::throwServiceException);
+                .orElseThrow(AVATAR_NOT_FOUND::throwServiceException);
 
 
         Member testAdmin = Member.create("admin", "admin@quizzle.com", defaultAvatar);
@@ -103,18 +130,18 @@ public class BaseInitData {
         if (avatarRepository.count() > 0) return;
 
         Avatar defaultAvatar = Avatar.builder()
-            .fileName("새콩이")
-            .url("https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EA%B8%B0%EB%B3%B8+%EC%95%84%EB%B0%94%ED%83%80.png")
-            .price(0)
-            .status(AvatarStatus.OWNED)
-            .build();
+                .fileName("새콩이")
+                .url("https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EA%B8%B0%EB%B3%B8+%EC%95%84%EB%B0%94%ED%83%80.png")
+                .price(0)
+                .status(AvatarStatus.OWNED)
+                .build();
 
         Avatar nerdySaekong = Avatar.builder()
-            .fileName("안경쓴 새콩이")
-            .url("https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EC%95%88%EA%B2%BD%EC%93%B4+%EC%83%88%EC%BD%A9%EC%9D%B4.png")
-            .price(300)
-            .status(AvatarStatus.AVAILABLE)
-            .build();
+                .fileName("안경쓴 새콩이")
+                .url("https://quizzle-avatars.s3.ap-northeast-2.amazonaws.com/%EC%95%88%EA%B2%BD%EC%93%B4+%EC%83%88%EC%BD%A9%EC%9D%B4.png")
+                .price(300)
+                .status(AvatarStatus.AVAILABLE)
+                .build();
 
         avatarRepository.save(defaultAvatar);
         avatarRepository.save(nerdySaekong);
