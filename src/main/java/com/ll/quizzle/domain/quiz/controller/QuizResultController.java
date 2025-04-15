7d9d7c49844ed1e008d1a0cce1eb37373f28a39a
jurio5 <sia5492@naver.com>
2025-04-16 07:20:52 +0900

    [chor] QuizResultController 라인 정리

--- src/main/java/com/ll/quizzle/domain/quiz/controller/QuizResultController.java
+++ src/main/java/com/ll/quizzle/domain/quiz/controller/QuizResultController.java
@@ -1,18 +1,26 @@
 package com.ll.quizzle.domain.quiz.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ll.quizzle.domain.member.service.MemberExpService;
 import com.ll.quizzle.domain.quiz.dto.request.QuizScoreRequest;
 import com.ll.quizzle.domain.quiz.dto.response.QuizResultResponse;
 import com.ll.quizzle.domain.quiz.service.QuizResultService;
-import com.ll.quizzle.domain.member.service.MemberExpService;
 import com.ll.quizzle.global.request.Rq;
 import com.ll.quizzle.global.response.RsData;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Quiz Result", description = "퀴즈 결과 조회 관련 API")
 @RequiredArgsConstructor
