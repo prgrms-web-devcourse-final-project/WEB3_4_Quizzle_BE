a4f2e5b2341e006443030d0f5cdd102bb80c4653
jurio5 <sia5492@naver.com>
2025-04-16 07:21:20 +0900

    [delete] 퀴즈 요청 dto 어노테이션 정리

--- src/main/java/com/ll/quizzle/domain/quiz/dto/request/QuizScoreRequest.java
+++ src/main/java/com/ll/quizzle/domain/quiz/dto/request/QuizScoreRequest.java
@@ -1,10 +1,5 @@
 package com.ll.quizzle.domain.quiz.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
 public record QuizScoreRequest(
         int score
 ) {}
